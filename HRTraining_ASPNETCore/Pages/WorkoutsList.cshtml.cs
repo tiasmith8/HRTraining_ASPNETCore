f62302173e35fa74a9789f04884e192e2393408d
Tia Smith <tsmith@onosys.com>
2022-08-29 11:34:59 -0400

    Add BindProperty flag instead of passing in the search term to the method

--- HRTraining_ASPNETCore/Pages/WorkoutsList.cshtml.cs
+++ HRTraining_ASPNETCore/Pages/WorkoutsList.cshtml.cs
@@ -1,18 +1,24 @@
 using System.Collections.Generic;
 using HRTraining.Core;
 using HRTraining.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 
 namespace HRTraining_ASPNETCore.Pages
 {
     public class WorkoutsListModel : PageModel
-    {
+    {        
         private readonly IConfiguration config;
         private readonly IWorkoutData workoutData;
-
+        
+        // Page model properties - output models
         public string Message { get; set; }
         public IEnumerable<Workout> Workouts { get; set; }
+        
+        // Output model to populate value for the search term to show the user what is being search on
+        [BindProperty(SupportsGet = true)] // By default only does POST requests
+        public string SearchTerm { get; set; }
 
         // IWorkoutData is a service that knows how to fetch workouts
         public WorkoutsListModel(IConfiguration config, IWorkoutData workoutData)
@@ -21,10 +27,10 @@ namespace HRTraining_ASPNETCore.Pages
             this.workoutData = workoutData;
         }
 
-        public void OnGet(string searchTerm) // ASP.NET Core will try to find something that is a searchTerm
+        public void OnGet()
         {
             Message = config["Message"];
-            Workouts = workoutData.GetWorkoutsByName(searchTerm);
+            Workouts = workoutData.GetWorkoutsByName(SearchTerm);
         }
     }
 }
